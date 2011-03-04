341f143e3d2836f566ef5388758575f88da2c71c
jbaris <juan.barisich@gmail.com>
2011-03-04 02:58:51 +0000

    Initial commit
    git-svn-id: http://jqa-project.googlecode.com/svn/trunk@3 11a24b44-ed4a-fc08-bf0b-2cb0453f03fa

---
+++ jqa-bce-api/src/main/java/ar/com/fluxit/jqa/bce/ClassFormatException.java
@@ -0,0 +1,23 @@
+package ar.com.fluxit.jqa.bce;
+
+public class ClassFormatException extends Exception {
+
+	public ClassFormatException() {
+		super();
+	}
+
+	public ClassFormatException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ClassFormatException(String message) {
+		super(message);
+	}
+
+	public ClassFormatException(Throwable cause) {
+		super(cause);
+	}
+
+	private static final long serialVersionUID = -7446697331694653195L;
+
+}
