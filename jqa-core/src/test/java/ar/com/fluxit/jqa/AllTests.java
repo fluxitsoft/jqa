2fdecd73c83d7a280e45ee1dc4cac6085a2448e7
jbaris <juan.barisich@gmail.com>
2011-03-04 02:59:36 +0000

    Initial commit
    git-svn-id: http://jqa-project.googlecode.com/svn/trunk@5 11a24b44-ed4a-fc08-bf0b-2cb0453f03fa

---
+++ jqa-core/src/test/java/ar/com/fluxit/jqa/AllTests.java
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ * JQA (http://code.google.com/p/jqa-project/)
+ * 
+ * Copyright (c) 2011 Juan Ignacio Barisich.
+ * 
+ * JQA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * JQA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package ar.com.fluxit.jqa;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import ar.com.fluxit.jqa.rule.AbstractionRuleTest;
+import ar.com.fluxit.jqa.rule.AllocationRuleTest;
+import ar.com.fluxit.jqa.rule.UsageRuleTest;
+import ar.com.fluxit.jqa.rule.AndRuleTest;
+import ar.com.fluxit.jqa.rule.InterfaceRuleTest;
+import ar.com.fluxit.jqa.rule.NamingRuleTest;
+import ar.com.fluxit.jqa.rule.NotRuleTest;
+import ar.com.fluxit.jqa.rule.OrRuleTest;
+import ar.com.fluxit.jqa.rule.TypingRuleTest;
+import ar.com.fluxit.jqa.rule.XorRuleTest;
+
+/**
+ * TODO javadoc
+ * 
+ * @author Juan Ignacio Barisich
+ */
+public class AllTests {
+
+	public static Test suite() {
+		final TestSuite suite = new TestSuite(AllTests.class.getName());
+		// $JUnit-BEGIN$
+		suite.addTestSuite(JQACheckerTest.class);
+		suite.addTestSuite(UsageRuleTest.class);
+		suite.addTestSuite(XorRuleTest.class);
+		suite.addTestSuite(OrRuleTest.class);
+		suite.addTestSuite(TypingRuleTest.class);
+		suite.addTestSuite(NamingRuleTest.class);
+		suite.addTestSuite(NotRuleTest.class);
+		suite.addTestSuite(AndRuleTest.class);
+		suite.addTestSuite(AbstractionRuleTest.class);
+		suite.addTestSuite(InterfaceRuleTest.class);	
+		suite.addTestSuite(AllocationRuleTest.class);
+		// $JUnit-END$
+		return suite;
+	}
+
+}
