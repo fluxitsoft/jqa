3b7fca00546252d8ed23c8461dbdb4a637cb8067
jbaris <juan.barisich@gmail.com>
2011-03-15 02:03:46 +0000

    RulesContextFactory added
    git-svn-id: http://jqa-project.googlecode.com/svn/trunk@61 11a24b44-ed4a-fc08-bf0b-2cb0453f03fa

---
+++ jqa-parent/jqa-ctx-factory-api/src/main/java/ar/com/fluxit/jqa/context/factory/RulesContextFactory.java
@@ -0,0 +1,14 @@
+package ar.com.fluxit.jqa.context.factory;
+
+import ar.com.fluxit.jqa.context.RulesContext;
+
+/**
+ * TODO javadoc
+ * 
+ * @author Juan Ignacio Barisich
+ */
+public interface RulesContextFactory {
+
+	RulesContext getRulesContext(Object source);
+
+}
