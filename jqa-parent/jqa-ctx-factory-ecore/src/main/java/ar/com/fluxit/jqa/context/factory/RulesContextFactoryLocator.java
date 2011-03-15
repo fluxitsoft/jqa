60c62a5ffead96b8f523a1e61b6ed4e1a108af7f
jbaris <juan.barisich@gmail.com>
2011-03-15 02:02:59 +0000

    RulesContextFactory added
    git-svn-id: http://jqa-project.googlecode.com/svn/trunk@59 11a24b44-ed4a-fc08-bf0b-2cb0453f03fa

---
+++ jqa-parent/jqa-ctx-factory-ecore/src/main/java/ar/com/fluxit/jqa/context/factory/RulesContextFactoryLocator.java
@@ -0,0 +1,14 @@
+package ar.com.fluxit.jqa.context.factory;
+
+/**
+ * TODO javadoc
+ * 
+ * @author Juan Ignacio Barisich
+ */
+public class RulesContextFactoryLocator {
+
+	public static RulesContextFactory getRulesContextFactory() {
+		return null;
+	}
+
+}
