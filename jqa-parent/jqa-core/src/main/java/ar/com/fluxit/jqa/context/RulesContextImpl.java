5d249121e900f1f3ab5ae411420730acc9ee70dd
jbaris <juan.barisich@gmail.com>
2011-04-10 18:13:32 +0000

    DefaultRulesContext renamed to RulesContextImpl
    git-svn-id: http://jqa-project.googlecode.com/svn/trunk@64 11a24b44-ed4a-fc08-bf0b-2cb0453f03fa

--- jqa-parent/jqa-core/src/main/java/ar/com/fluxit/jqa/context/RulesContextImpl.java
+++ jqa-parent/jqa-core/src/main/java/ar/com/fluxit/jqa/context/RulesContextImpl.java
@@ -13,12 +13,12 @@ import ar.com.fluxit.jqa.rule.RuleSet;
  * 
  * @author Juan Ignacio Barisich
  */
-public class DefaultRulesContext implements RulesContext {
+public class RulesContextImpl implements RulesContext {
 
 	private final Collection<RuleSet> ruleSets;
 	private final Map<String, Predicate> globalPredicates;
 
-	public DefaultRulesContext() {
+	public RulesContextImpl() {
 		ruleSets = new ArrayList<RuleSet>();
 		globalPredicates = new HashMap<String, Predicate>();
 	}
