f141504baea2550b32819d8e7a6dc49a1e7be83c
jbaris <juan.barisich@gmail.com>
2011-04-26 21:41:20 +0000

    jqa-test-project refactoring
    git-svn-id: http://jqa-project.googlecode.com/svn/trunk@110 11a24b44-ed4a-fc08-bf0b-2cb0453f03fa

---
+++ jqa-parent/jqa-test-parent/jqa-test-project/src/test/java/ar/com/fluxit/jqa/test/bo/CarBO.java
@@ -0,0 +1,7 @@
+package ar.com.fluxit.jqa.test.bo;
+
+import ar.com.fluxit.jqa.test.fmk.bo.BussinesObject;
+
+public interface CarBO extends BussinesObject {
+
+}
---
+++ jqa-parent/jqa-test-parent/jqa-test-project/src/test/java/ar/com/fluxit/jqa/test/bo/impl/CarBOImpl.java
@@ -0,0 +1,7 @@
+package ar.com.fluxit.jqa.test.bo.impl;
+
+import ar.com.fluxit.jqa.test.bo.CarBO;
+
+public class CarBOImpl implements CarBO {
+
+}
