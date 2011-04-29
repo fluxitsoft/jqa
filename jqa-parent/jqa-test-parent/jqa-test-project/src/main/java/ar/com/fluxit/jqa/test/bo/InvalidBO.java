da697714a3c68ad334ded231b1142627cacd0007
jbaris <juan.barisich@gmail.com>
2011-04-29 00:41:13 +0000

    Test runner added
    git-svn-id: http://jqa-project.googlecode.com/svn/trunk@145 11a24b44-ed4a-fc08-bf0b-2cb0453f03fa

---
+++ jqa-parent/jqa-test-parent/jqa-test-project/src/main/java/ar/com/fluxit/jqa/test/bo/InvalidBO.java
@@ -0,0 +1,10 @@
+package ar.com.fluxit.jqa.test.bo;
+
+/**
+ * TODO javadoc
+ * 
+ * @author Juan Ignacio Barisich
+ */
+public interface InvalidBO {
+
+}
---
+++ jqa-parent/jqa-test-parent/jqa-test-project/src/main/java/ar/com/fluxit/jqa/test/dao/InvalidDAO.java
@@ -0,0 +1,10 @@
+package ar.com.fluxit.jqa.test.dao;
+
+/**
+ * TODO javadoc
+ * 
+ * @author Juan Ignacio Barisich
+ */
+public class InvalidDAO {
+
+}
