35c35ae600143de551fdae15c00c6959dd4f9cac
jbaris <juan.barisich@gmail.com>
2011-05-06 14:32:33 +0000

    Tests for services
    git-svn-id: http://jqa-project.googlecode.com/svn/trunk@197 11a24b44-ed4a-fc08-bf0b-2cb0453f03fa

---
+++ jqa-parent/jqa-test-parent/jqa-test-project/src/main/java/ar/com/fluxit/jqa/test/service/CarService.java
@@ -0,0 +1,10 @@
+package ar.com.fluxit.jqa.test.service;
+
+/**
+ * TODO javadoc
+ * 
+ * @author Juan Ignacio Barisich
+ */
+public interface CarService {
+
+}
---
+++ jqa-parent/jqa-test-parent/jqa-test-project/src/main/java/ar/com/fluxit/jqa/test/service/MotorcycleService.java
@@ -0,0 +1,12 @@
+package ar.com.fluxit.jqa.test.service;
+
+import ar.com.fluxit.jqa.test.fmk.service.Service;
+
+/**
+ * TODO javadoc
+ * 
+ * @author Juan Ignacio Barisich
+ */
+public class MotorcycleService implements Service {
+
+}
