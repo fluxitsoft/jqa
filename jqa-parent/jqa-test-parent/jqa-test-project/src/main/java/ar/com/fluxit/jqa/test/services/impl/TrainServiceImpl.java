b9d76eba0c0dfb8e3d79885dd106ad366aa5bdc5
jbaris <juan.barisich@gmail.com>
2011-05-06 14:34:07 +0000

    Tests for services
    git-svn-id: http://jqa-project.googlecode.com/svn/trunk@198 11a24b44-ed4a-fc08-bf0b-2cb0453f03fa

---
+++ jqa-parent/jqa-test-parent/jqa-test-project/src/main/java/ar/com/fluxit/jqa/test/services/impl/TrainServiceImpl.java
@@ -0,0 +1,12 @@
+package ar.com.fluxit.jqa.test.services.impl;
+
+import ar.com.fluxit.jqa.test.services.TrainService;
+
+/**
+ * TODO javadoc
+ * 
+ * @author Juan Ignacio Barisich
+ */
+public class TrainServiceImpl implements TrainService {
+
+}
