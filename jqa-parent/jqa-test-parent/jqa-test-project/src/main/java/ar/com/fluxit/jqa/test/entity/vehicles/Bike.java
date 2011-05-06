6def1e9782dfec56aad18175ce895aee4f8b6a1d
jbaris <juan.barisich@gmail.com>
2011-05-06 16:27:27 +0000

    Tests for entities
    git-svn-id: http://jqa-project.googlecode.com/svn/trunk@207 11a24b44-ed4a-fc08-bf0b-2cb0453f03fa

---
+++ jqa-parent/jqa-test-parent/jqa-test-project/src/main/java/ar/com/fluxit/jqa/test/entity/vehicles/Bike.java
@@ -0,0 +1,12 @@
+package ar.com.fluxit.jqa.test.entity.vehicles;
+
+import ar.com.fluxit.jqa.test.fmk.entity.Entity;
+
+/**
+ * TODO javadoc
+ * 
+ * @author Juan Ignacio Barisich
+ */
+public class Bike implements Entity {
+
+}
---
+++ jqa-parent/jqa-test-parent/jqa-test-project/src/main/java/ar/com/fluxit/jqa/test/entity/vehicles/Motorcycle.java
@@ -0,0 +1,10 @@
+package ar.com.fluxit.jqa.test.entity.vehicles;
+
+/**
+ * TODO javadoc
+ * 
+ * @author Juan Ignacio Barisich
+ */
+public class Motorcycle {
+
+}
