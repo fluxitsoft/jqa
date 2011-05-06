6943cffba6a42fcaa0a8d4b3b3f8422406f2cde9
jbaris <juan.barisich@gmail.com>
2011-05-06 16:26:25 +0000

    Tests for entities
    git-svn-id: http://jqa-project.googlecode.com/svn/trunk@204 11a24b44-ed4a-fc08-bf0b-2cb0453f03fa

---
+++ jqa-parent/jqa-test-parent/jqa-test-project/src/main/java/ar/com/fluxit/jqa/test/entity/vehicles/old/Train.java
@@ -0,0 +1,12 @@
+package ar.com.fluxit.jqa.test.entity.vehicles.old;
+
+import ar.com.fluxit.jqa.test.fmk.entity.Entity;
+
+/**
+ * TODO javadoc
+ * 
+ * @author Juan Ignacio Barisich
+ */
+public class Train implements Entity {
+
+}
