216c5c4b1b74b6dc1e3dbfd18d37ed5b2ca10e4c
Juan Ignacio Barisich <juan.barisich@gmail.com>
2012-04-14 15:11:34 -0300

    new file:   jqa-ctx-factory-xtext/.classpath 	new file:   jqa-ctx-factory-xtext/.project 	new file:   jqa-ctx-factory-xtext/.settings/org.eclipse.jdt.core.prefs 	new file:   jqa-ctx-factory-xtext/pom.xml 	new file:   jqa-ctx-factory-xtext/src/main/java/ar/com/fluxit/jqa/context/factory/RulesContextFactoryLocator.java 	new file:   jqa-ctx-factory-xtext/src/main/java/ar/com/fluxit/jqa/context/factory/xtext/RulesContextFactoryImpl.java 	new file:   jqa-ctx-factory-xtext/target/classes/ar/com/fluxit/jqa/context/factory/RulesContextFactoryLocator.class 	new file:   jqa-ctx-factory-xtext/target/classes/ar/com/fluxit/jqa/context/factory/xtext/RulesContextFactoryImpl.class

---
+++ jqa-parent/jqa-ctx-factory-xtext/src/main/java/ar/com/fluxit/jqa/context/factory/xtext/RulesContextFactoryImpl.java
@@ -0,0 +1,24 @@
+package ar.com.fluxit.jqa.context.factory.xtext;
+
+import java.io.File;
+import java.io.InputStream;
+
+import ar.com.fluxit.jqa.context.RulesContext;
+import ar.com.fluxit.jqa.context.factory.RulesContextFactory;
+import ar.com.fluxit.jqa.context.factory.exception.RulesContextFactoryException;
+
+public class RulesContextFactoryImpl implements RulesContextFactory {
+
+	@Override
+	public RulesContext getRulesContext(File source) throws RulesContextFactoryException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public RulesContext getRulesContext(InputStream source) throws RulesContextFactoryException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
