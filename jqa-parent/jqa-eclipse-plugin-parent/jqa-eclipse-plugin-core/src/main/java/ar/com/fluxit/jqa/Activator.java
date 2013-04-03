f0b9b5635d8b38ea1044cf2520376ed2b0353ed4
Juan Ignacio Barisich <juan.barisich@gmail.com>
2013-04-03 17:25:23 -0300

    modified:   jqa-parent/jqa-eclipse-plugin-parent/jqa-eclipse-plugin-core/plugin.xml 	modified:   jqa-parent/jqa-eclipse-plugin-parent/jqa-eclipse-plugin-core/src/main/java/ar/com/fluxit/jqa/Activator.java 	deleted:    jqa-parent/jqa-eclipse-plugin-parent/jqa-eclipse-plugin-core/src/main/java/ar/com/fluxit/jqa/actions/SampleAction.java 	jqa-parent/jqa-eclipse-plugin-parent/jqa-eclipse-plugin-core/icons/ok.png 	jqa-parent/jqa-eclipse-plugin-parent/jqa-eclipse-plugin-core/src/main/java/ar/com/fluxit/jqa/actions/ 	jqa-parent/jqa-eclipse-plugin-parent/jqa-eclipse-plugin-core/src/main/java/ar/com/fluxit/jqa/wizard/

--- jqa-parent/jqa-eclipse-plugin-parent/jqa-eclipse-plugin-core/src/main/java/ar/com/fluxit/jqa/Activator.java
+++ jqa-parent/jqa-eclipse-plugin-parent/jqa-eclipse-plugin-core/src/main/java/ar/com/fluxit/jqa/Activator.java
@@ -1,3 +1,21 @@
+/*******************************************************************************
+ * JQA (http://github.com/jbaris/jqa)
+ * 
+ * Copyright (c) 2011 Juan Ignacio Barisich.
+ * 
+ * JQA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * JQA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with JQA.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 package ar.com.fluxit.jqa;
 
 import org.eclipse.jface.resource.ImageDescriptor;
@@ -6,56 +24,35 @@ import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
+ * 
+ * @author Juan Ignacio Barisich
  */
 public class Activator extends AbstractUIPlugin {
 
-	// The plug-in ID
-	public static final String PLUGIN_ID = "jqa-eclipse-plugin"; //$NON-NLS-1$
-
-	// The shared instance
+	public static final String PLUGIN_ID = "jqa-eclipse-plugin";
 	private static Activator plugin;
-	
-	/**
-	 * The constructor
-	 */
+
+	public static Activator getDefault() {
+		return plugin;
+	}
+
+	public static ImageDescriptor getImageDescriptor(String path) {
+		return imageDescriptorFromPlugin(PLUGIN_ID, path);
+	}
+
 	public Activator() {
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
+	@Override
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
+	@Override
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
 		super.stop(context);
 	}
 
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
-	public static Activator getDefault() {
-		return plugin;
-	}
-
-	/**
-	 * Returns an image descriptor for the image file at the given
-	 * plug-in relative path
-	 *
-	 * @param path the path
-	 * @return the image descriptor
-	 */
-	public static ImageDescriptor getImageDescriptor(String path) {
-		return imageDescriptorFromPlugin(PLUGIN_ID, path);
-	}
 }
